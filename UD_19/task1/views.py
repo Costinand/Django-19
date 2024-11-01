3e2940297b7ea09fdc74a92664c7c2d7cfbae1a0
сostinand <tras_co@mail.ru>
2024-11-01 14:02:41 +0200

    01.11 19_3 ready

--- UD_19/task1/views.py
+++ UD_19/task1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from .forms import UserRegister
+from task1.models import Buyer
 
 
 # Create your views here.
@@ -15,21 +16,25 @@ def cart(request):
     return render(request, 'cart.html')
 
 def sign_up(request):
-    users = ['Ivan92', 'jonnaZ', 'Masha_K', 'LOTR',]
+    name_users = []  # список имен пользователей
+    users = Buyer.objects.all() # список объектов из таблицы покупателей
+    for user in users: # перебор объектов
+        name_users.append(user.name) # в список покупателей достаются только имена объектов
+    print(name_users) # для наглядности вывод на терминале
     info = {}
     if request.method == 'POST':
-        user_exist = False
+        user_exist = False # по умолчанию новый пользователь не присутствует
         form = UserRegister(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
+        if form.is_valid():   # если форма запроса заполнена
+            username = form.cleaned_data['username'] # сбор каждого поля для обработки
             password = form.cleaned_data['password']
             repeat_password = form.cleaned_data['repeat_password']
             age = int(form.cleaned_data['age'])
 
-            is_user = username in users
-            if password == repeat_password:
-                if age >= 18:
-                    if is_user == False:
+            is_user = username in name_users  # пользователь - это кто-то среди имен пользователей
+            if password == repeat_password:   # если пароли совпадают
+                if age >= 18:                 # и если возраст соответстует
+                    if is_user == False:     #  и если нету (34) то его присутсятвие меняется на положительное (26)
                         user_exist = True
                     else:
                         info['error'] = 'Пользователь уже существует'
@@ -37,8 +42,9 @@ def sign_up(request):
                     info['error'] = 'Вы должны быть старше 18'
             else:
                 info['error'] = 'Пароли не совпадают'
-            if user_exist:
+            if user_exist:             # итак он имеет право присутствовать в пользователях(37)
                 message = (f"Приветствуем, {username}")
+                Buyer.objects.create(name=username, balance=0, age=age)  # добавление в базу
                 print(message)
             else:
                 message = info['error']
@@ -47,3 +53,4 @@ def sign_up(request):
     else:
         form = UserRegister()
     return render(request, 'registration_page.html', info)
+
